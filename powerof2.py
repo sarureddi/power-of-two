8d073035de38ad372303483770c37ff0213d9d50
saru <sarumathiayyappan@gmail.com>
2019-06-06 14:28:04 +0530

    Create powerof2.py

---
+++ powerof2.py
@@ -0,0 +1,10 @@
+def ispowertwo(x)
+return (x and (not(x & (x - 1))) ) 
+if(isPowerOfTwo(31)): 
+    print('Yes') 
+else: 
+    print('No') 
+if(isPowerOfTwo(64)): 
+    print('Yes') 
+else: 
+    print('No') 
